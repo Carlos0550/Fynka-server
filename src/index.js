a3314eb4e11f179a363d10b60f9f87d7f97cea2b
carlos0550 <carlospelinski03@gmail.com>
2024-11-23 01:48:04 -0300

    Ajustes de logica en el inicio de sesión y registro

--- src/index.js
+++ src/index.js
@@ -8,9 +8,11 @@ const dayjs = require("dayjs")
 const utc = require("dayjs/plugin/utc.js")
 const timezone = require("dayjs/plugin/timezone.js");
 const bcryptjs = require("bcryptjs")
-const jwt = require("jsonwebtoken");
+
+const { v4: uuidv4 } = require('uuid');
 dayjs.extend(utc)
 dayjs.extend(timezone)
+dayjs.tz.setDefault("America/Argentina/Buenos_Aires")
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(express.text())
@@ -25,63 +27,134 @@ app.get("/", (req, res) => {
     res.status(200).send("SERVIDOR ON")
 });
 
-const verifyPass = async(psw, hash) =>{
+const verifyPass = async (psw, hash) => {
     return bcryptjs.compare(psw, hash)
 }
 
-app.post("/create-business",upload.none(), async(req,res)=> {
-    const {email,username,psw} = req.body
+app.post("/create-administrator", upload.none(), async (req, res) => {
+    const { email, username, psw } = req.body
     const saltRounds = 10
 
-    if(!email || !username || !psw) return res.status(400).json({msg: "El servidor no recibió correctamente algunos datos, verifica que todo esté en orden."})
-    if(psw.length > 15) return res.status(400).json({msg:"La contraseña no puede ser mayor a 15 caractéres."})
-        
+    if (!email || !username || !psw) return res.status(400).json({ msg: "El servidor no recibió correctamente algunos datos, verifica que todo esté en orden." })
+    if (psw.length > 15) return res.status(400).json({ msg: "La contraseña no puede ser mayor a 15 caractéres." })
+
     const client = await clientDb.connect()
-    const insertQuery = `
-        INSERT INTO credenciales(email, nombre_usuario, contrasena) VALUES($1,$2,$3)
+    const findAdminQuery = `
+        SELECT * FROM credenciales WHERE nombre_usuario = $1 OR email = $2
     `
+
+    const insertQuery = `
+        INSERT INTO credenciales(email, nombre_usuario, contrasena, administrador, empleado) VALUES($1,$2,$3,$4,$5) RETURNING *
+    `
+    const insertQuery1 = `
+        INSERT INTO sucursales(nombre, administrador_id) VALUES ($1, $2)
+    `
+
     try {
+        await client.query("BEGIN")
+        const result = await client.query(findAdminQuery, [username, email])
+
+        if (result.rowCount > 0) {
+            await client.query("ROLLBACK")
+            return res.status(400).json({ msg: "El nombre de usuario o correo ya está usado." })
+        }
+
         const hashedPassword = await bcryptjs.hash(psw, saltRounds)
-        const result = await client.query(insertQuery,[email,username,hashedPassword])
-        if(result.rowCount === 0) return res.status(500).json({msg: "Ocurrió un error inesperado, por favor intente nuevamente."})
-        return res.status(200).json({msg: "Empresa registrada correctamente."})
-        
+
+        console.log(email, username, "hashedPassword", true, false)
+        const result1 = await client.query(insertQuery, [email, username, hashedPassword, true, false])
+
+        if (result1.rowCount === 0) {
+            await client.query("ROLLBACK")
+            return res.status(500).json({ msg: "Ocurrió un error inesperado, por favor intente nuevamente." })
+        }
+        const nombreSucursal = `Sucursal-${uuidv4()}`
+        const administrador_id = result1.rows[0].id
+        console.log("ADMINISTRADOR ID: ", administrador_id)
+        const result2 = await client.query(insertQuery1, [nombreSucursal, administrador_id])
+
+        if (result2.rowCount === 0) {
+            await client.query("ROLLBACK")
+
+            return res.status(400).json({ msg: "Ocurrió un error inesperado al crear su usuario, por favor intente nuevamente." })
+        }
+        await client.query("COMMIT")
+        return res.status(200).json({ msg: "Empresa registrada correctamente." })
+
     } catch (error) {
         console.log(error)
-        return res.status(500).json({msg: "Error interno del servidor, espere unos segundos e intente nuevamente."})
-    }finally{
+        await client.query("ROLLBACK")
+        return res.status(500).json({ msg: "Error interno del servidor, espere unos segundos e intente nuevamente." })
+    } finally {
         client.release()
     }
-    
+
 });
 
-app.post("/login-user", upload.none(), async(req,res)=>{
-    const {email,username,psw} = req.body
+app.post("/login-user", upload.none(), async (req, res) => {
+    const { email, username, psw } = req.body
 
-    if((!email || !username) && !psw) return res.status(400).json({msg: "El servidor no recibió correctamente algunos datos, verifica que todo esté en orden."})
-    
-    const getQuery = `
-        SELECT * FROM credenciales WHERE nombre_usuario = $1 OR email = $2;
+    if ((!email || !username) && !psw) return res.status(400).json({ msg: "El servidor no recibió correctamente algunos datos, verifica que todo esté en orden." })
+
+    const getQuery1 = `
+        SELECT id, contrasena, empleado, administrador FROM credenciales WHERE nombre_usuario = $1 OR email = $2;
+    `
+
+    const updateQuery = `
+        UPDATE credenciales SET fecha_inicio_sesion = $1, fecha_reestablecer_autenticacion = $2, autenticado = $3 WHERE id = $4 RETURNING *
     `
     const client = await clientDb.connect()
 
     try {
-        const response1 = await client.query(getQuery,[username, email])
-        if(response1.rowCount === 0) return res.status(404).json({msg: "El usuario no existe en nuestros registros."})
-        
+        await client.query("BEGIN")
+        const response1 = await client.query(getQuery1, [username, email])
+        if (response1.rowCount === 0) return res.status(404).json({ msg: "El usuario no existe en nuestros registros." })
+
         const userPassword = response1.rows[0].contrasena
-        console.log(userPassword)
+        const userId = response1.rows[0].id
+
+        const today = dayjs()
+        const tomorrow = dayjs().add(1, "day")
 
         const isPswMatch = await verifyPass(psw, userPassword)
 
-        console.log(isPswMatch)
-        if(isPswMatch) return res.status(200).json({msg: "Bienvenido a Fynka"})
-        return res.status(401).json({msg: "La contraseña introducida no es correcta."})
+        if (!isPswMatch){
+            await client.query("ROLLBACK")
+            return res.status(401).json({ msg: "La contraseña introducida no es correcta." })
+        }
+
+        const response2 = await client.query(updateQuery, [today, tomorrow, true, userId])
+    
+        if (response2.rowCount === 0){
+            await client.query("ROLLBACK")
+            return res.status(400).json({msg: "Ocurrió algo inesperado al iniciar sesión, espere unos segundos e intente nuevamente."})
+        }
+        const { contrasena, fecha_inicio_sesion, fecha_reestablecer_autenticacion, ...valuesToReturn } = response2.rows[0]
+        await client.query("COMMIT")
+        return res.status(200).json({ msg: "Bienvenido a Fynka", usrData: valuesToReturn })
 
     } catch (error) {
+        await client.query("ROLLBACK")
         console.log(error)
-        return res.status(500).json({msg: "Error interno del servidor, espere unos segundos e intente nuevamente."})
-    }finally{
+        return res.status(500).json({ msg: error.message || "Error interno del servidor, espere unos segundos e intente nuevamente." })
+    } finally {
+        client.release()
+    }
+})
+
+cron.schedule("* * * * *", async () => {
+    const query1 = `UPDATE credenciales SET autenticado = false WHERE DATE(fecha_reestablecer_autenticacion) >= $1`
+    const client = await clientDb.connect()
+    try {
+        await client.query("BEGIN")
+        await client.query(query1, [dayjs().format("YYYY-MM-DD")])
+        await client.query("COMMIT")
+        console.log("TAREAS CRON FINALIZADAS SIN PROBLEMAS")
+    } catch (error) {
+        await client.query("ROLLBACK")
+        console.log(error)
+        console.log(error.message)
+    } finally {
         client.release()
     }
 })
