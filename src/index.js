999242f06928d63393d15ae5f966d7d5f64db72c
carlos0550 <carlospelinski03@gmail.com>
2024-11-25 14:06:27 -0300

    Ajustes de logica en el endpoint de verificacion de usuario. cron jobs y otros endpoints

--- src/index.js
+++ src/index.js
@@ -31,8 +31,8 @@ const verifyPass = async (psw, hash) => {
     return bcryptjs.compare(psw, hash)
 }
 
-app.post("/create-administrator", upload.none(), async (req, res) => {
-    const { email, username, psw } = req.body
+app.post("/create-user", upload.none(), async (req, res) => {
+    const { email, psw } = req.body
     const saltRounds = 10
 
     if (!email || !username || !psw) return res.status(400).json({ msg: "El servidor no recibió correctamente algunos datos, verifica que todo esté en orden." })
@@ -40,11 +40,11 @@ app.post("/create-administrator", upload.none(), async (req, res) => {
 
     const client = await clientDb.connect()
     const findAdminQuery = `
-        SELECT * FROM credenciales WHERE nombre_usuario = $1 OR email = $2
+        SELECT * FROM usuarios WHERE email = $1
     `
 
     const insertQuery = `
-        INSERT INTO credenciales(email, nombre_usuario, contrasena, administrador, empleado) VALUES($1,$2,$3,$4,$5) RETURNING *
+        INSERT INTO usuarios(email, nombre_usuario, contrasena, administrador, empleado) VALUES($1,$2,$3,$4,$5) RETURNING *
     `
     const insertQuery1 = `
         INSERT INTO sucursales(nombre, administrador_id) VALUES ($1, $2)
@@ -52,7 +52,7 @@ app.post("/create-administrator", upload.none(), async (req, res) => {
 
     try {
         await client.query("BEGIN")
-        const result = await client.query(findAdminQuery, [username, email])
+        const result = await client.query(findAdminQuery, [email])
 
         if (result.rowCount > 0) {
             await client.query("ROLLBACK")
@@ -61,7 +61,6 @@ app.post("/create-administrator", upload.none(), async (req, res) => {
 
         const hashedPassword = await bcryptjs.hash(psw, saltRounds)
 
-        console.log(email, username, "hashedPassword", true, false)
         const result1 = await client.query(insertQuery, [email, username, hashedPassword, true, false])
 
         if (result1.rowCount === 0) {
@@ -70,7 +69,6 @@ app.post("/create-administrator", upload.none(), async (req, res) => {
         }
         const nombreSucursal = `Sucursal-${uuidv4()}`
         const administrador_id = result1.rows[0].id
-        console.log("ADMINISTRADOR ID: ", administrador_id)
         const result2 = await client.query(insertQuery1, [nombreSucursal, administrador_id])
 
         if (result2.rowCount === 0) {
@@ -92,22 +90,22 @@ app.post("/create-administrator", upload.none(), async (req, res) => {
 });
 
 app.post("/login-user", upload.none(), async (req, res) => {
-    const { email, username, psw } = req.body
+    const { email, psw } = req.body
 
-    if ((!email || !username) && !psw) return res.status(400).json({ msg: "El servidor no recibió correctamente algunos datos, verifica que todo esté en orden." })
+    if ((!email) && !psw) return res.status(400).json({ msg: "El servidor no recibió correctamente algunos datos, verifica que todo esté en orden." })
 
     const getQuery1 = `
-        SELECT id, contrasena, empleado, administrador FROM credenciales WHERE nombre_usuario = $1 OR email = $2;
+        SELECT id, contrasena, empleado, administrador FROM usuarios WHERE email = $1;
     `
 
     const updateQuery = `
-        UPDATE credenciales SET fecha_inicio_sesion = $1, fecha_reestablecer_autenticacion = $2, autenticado = $3 WHERE id = $4 RETURNING *
+        UPDATE usuarios SET fecha_inicio_sesion = $1, fecha_reestablecer_autenticacion = $2, autenticado = $3 WHERE id = $4 RETURNING *
     `
     const client = await clientDb.connect()
 
     try {
         await client.query("BEGIN")
-        const response1 = await client.query(getQuery1, [username, email])
+        const response1 = await client.query(getQuery1, [email])
         if (response1.rowCount === 0) return res.status(404).json({ msg: "El usuario no existe en nuestros registros." })
 
         const userPassword = response1.rows[0].contrasena
@@ -143,30 +141,50 @@ app.post("/login-user", upload.none(), async (req, res) => {
 });
 
 app.get("/verifyAuthUser", async (req, res) => {
-    const { username, email } = req.query
+    const {  email } = req.query
 
-    if (!username || !email) return res.status(400).json({ msg: "El servidor no pudo validar su sesión" })
+    if (!email) return res.status(400).json({ msg: "El servidor no pudo validar su sesión" })
 
     const client = await clientDb.connect()
-    const query1 = `SELECT * FROM credenciales WHERE nombre_usuario = $1 OR email = $2`
+    const query1 = `SELECT * FROM usuarios WHERE email = $1`
     try {
-        const response = await client.query(query1, [username, email])
+        const response = await client.query(query1, [email])
         if (response.rowCount === 0) return res.status(400).json({ msg: "El servidor no pudo encontrar su usuario, por favor inicie sesión nuevamente." })
         const auth = response.rows[0].autenticado
-        console.log("Autenticado: ",auth)
-        console.log("Usuario: ", response.rows)
-        if(!auth) return res.status(401).json({msg: "Su sesión caducó, por favor inicie sesión nuevamente."})
-        return res.status(200).json({usrData: response.rows[0]})
+
+        if (!auth) return res.status(401).json({ msg: "Su sesión caducó, por favor inicie sesión nuevamente." })
+        return res.status(200).json({ usrData: response.rows[0] })
     } catch (error) {
         console.log(error)
-        return res.status(500).json({ msg:"Error interno del servidor, espere unos segundos e intente nuevamente." })
+        return res.status(500).json({ msg: "Error interno del servidor, espere unos segundos e intente nuevamente." })
     } finally {
         client.release()
     }
-})
+});
+
+app.get("/get-user_info", async (req, res) => {
+    const { userID } = req.query
+
+    if (!userID) return res.status(400).json({ msg: "El servidor no recibió correctamente algún dato" })
+    const client = await clientDb.connect()
+
+    const query1 = `SELECT * FROM usuarios WHERE id = $1`
+    try {
+        const response = await client.query(query1, [userID])
+        console.log(userID)
+        console.log(response.rows)
+        if (response.rowCount === 0) return res.status(404).json({ msg: "Deberá completar su perfil para hacer uso del sistema." })
+        return res.status(200).json({ usrInfo: response.rows[0] })
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({ msg: "Error interno del servidor, espere unos segundos e intente nuevamente." })
+    } finally {
+        client.release()
+    }
+});
 
 cron.schedule("*/30 * * * *", async () => {
-    const query1 = `UPDATE credenciales SET autenticado = false WHERE DATE(fecha_reestablecer_autenticacion) = $1`
+    const query1 = `UPDATE usuarios SET autenticado = false WHERE DATE(fecha_reestablecer_autenticacion) = $1`
     const client = await clientDb.connect()
     try {
         await client.query("BEGIN")
